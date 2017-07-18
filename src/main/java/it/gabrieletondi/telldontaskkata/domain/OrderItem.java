32e12db50d9daef0b3641ce3d20174e3130fbfd6
Alvaro Garcia <derecerca@gmail.com>
2017-07-18 02:28:07 +0200

    Refactor: remove dead code

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
@@ -21,16 +21,8 @@ public class OrderItem {
         this.quantity = quantity;
     }
 
-    public Product getProduct() {
-        return product;
-    }
-
     public void setProduct(Product product) {
         this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
     }
 
     public void setQuantity(int quantity) {
