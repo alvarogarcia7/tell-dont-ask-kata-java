94548225fc47dd6b47697c83091e683623e7f9ed
Alvaro Garcia <derecerca@gmail.com>
2017-07-18 02:09:06 +0200

    Refactor: inline method

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -1,16 +1,12 @@
 package it.gabrieletondi.telldontaskkata.useCase;
 
 import it.gabrieletondi.telldontaskkata.domain.Order;
-import it.gabrieletondi.telldontaskkata.domain.OrderItem;
 import it.gabrieletondi.telldontaskkata.domain.Product;
 import it.gabrieletondi.telldontaskkata.exception.UnknownProductException;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
 import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
 
-import java.math.BigDecimal;
-
 import static java.math.BigDecimal.valueOf;
-import static java.math.RoundingMode.HALF_UP;
 
 public class OrderCreationUseCase {
     private final OrderRepository orderRepository;
@@ -31,14 +27,11 @@ public class OrderCreationUseCase {
             if (product == null) {
                 throw new UnknownProductException();
             } else {
-                calculateTaxForOrderItem(order, product, itemRequest.getQuantity());
+                order.add(product, itemRequest.getQuantity());
             }
         }
 
         orderRepository.save(order);
     }
 
-    private void calculateTaxForOrderItem (final Order order, final Product product, final int quantity) {
-        order.add(product, quantity);
-    }
 }
