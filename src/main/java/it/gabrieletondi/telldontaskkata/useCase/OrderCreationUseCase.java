5b90b6754e971ce366038d9abda1e7c4e9c0fa3d
Alvaro Garcia <derecerca@gmail.com>
2017-07-18 02:46:25 +0200

    Refactor: remove dead code

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -4,7 +4,6 @@ import it.gabrieletondi.telldontaskkata.domain.Order;
 import it.gabrieletondi.telldontaskkata.domain.Product;
 import it.gabrieletondi.telldontaskkata.exception.UnknownProductException;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
-import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
 
 import static java.math.BigDecimal.valueOf;
 
