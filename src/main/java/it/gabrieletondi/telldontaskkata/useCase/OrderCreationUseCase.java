e1bfa953a21583537c02701710db8d7b31a54142
Alvaro Garcia <derecerca@gmail.com>
2017-07-19 23:09:56 +0200

    Refactor: remove dead code

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -2,11 +2,7 @@ package it.gabrieletondi.telldontaskkata.useCase;
 
 import it.gabrieletondi.telldontaskkata.domain.Order;
 import it.gabrieletondi.telldontaskkata.domain.OrderItem;
-import it.gabrieletondi.telldontaskkata.domain.Product;
-import it.gabrieletondi.telldontaskkata.exception.UnknownProductException;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
-
-import static java.math.BigDecimal.valueOf;
 
 public class OrderCreationUseCase {
     private final OrderRepository orderRepository;
@@ -21,12 +17,7 @@ public class OrderCreationUseCase {
 
         for (SellItemRequest itemRequest : request.getRequests()) {
             OrderItem request1 = itemRequest.getRequest();
-
-            if (request1.isInvalid()) {
-                throw new UnknownProductException();
-            } else {
-                order.add(request1);
-            }
+            order.add(request1);
         }
 
         orderRepository.save(order);
