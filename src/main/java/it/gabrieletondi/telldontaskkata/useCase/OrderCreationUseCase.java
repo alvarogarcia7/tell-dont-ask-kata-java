2a150302e2a78277503004f209b5d207bc4c0899
Alvaro Garcia <derecerca@gmail.com>
2017-07-19 23:30:25 +0200

    Refactor: prefer using a more functional style (reduce)

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -3,6 +3,11 @@ package it.gabrieletondi.telldontaskkata.useCase;
 import it.gabrieletondi.telldontaskkata.domain.Order;
 import it.gabrieletondi.telldontaskkata.domain.OrderItem;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
+import org.mockito.internal.matchers.Or;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 public class OrderCreationUseCase {
     private final OrderRepository orderRepository;
@@ -12,9 +17,13 @@ public class OrderCreationUseCase {
     }
 
     public void run(SellItemRequest.SellItemsRequest request) {
-        Order order = new Order();
-
-        request.getRequests().forEach(itemRequest -> order.add(itemRequest.getRequest()));
+        BiFunction <Order, SellItemRequest, Order> f = (order1, sellItemRequest) ->
+        {
+            order1.add(sellItemRequest.getRequest());
+            return order1;
+        };
+        BinaryOperator<Order> combiner = (order1, order2) -> order2;
+        Order order = request.getRequests().stream().reduce(new Order(),f, combiner);
 
         orderRepository.save(order);
     }
